48156ff7618283cae5b7bd8d6beadea6e1a60b72
kasumoto <hasmoto.0112@gmail.com>
2020-09-21 22:52:26 +0900

    Finish Chapter03-02

--- app.js
+++ app.js
@@ -47,27 +47,74 @@ function getFromClient(request, response) {
     }
 }
 
+var data = { msg: 'no messaga...' };
 //indexのアクセス処理
-var data = {
-    'Taro' : '09-999-999',
-    'Hanako' : '09-999-5432',
-    'Maruto' : '09-999-325',
-    'Katuro' : '09-999-555'
-};
-
 function response_index(request, response) {
-    var msg = 'これはIndexページです。';
+
+    //POST時のアクセス処理
+    if (request.method == 'POST') {
+        var body = '';
+
+        //データ受信時のイベント処理
+        request.on('data', (data) => {
+            body += data;
+        });
+
+        //データ受信終了のイベント処理
+        request.on('end', () => {
+            data = qs.parse(body);
+            //クッキーの保存
+            setCookie('msg', data.msg, response);
+            write_index(request, response);
+        });
+    } else {
+        write_index(request, response);
+    }
+}
+
+//indexページ作成
+function write_index(request, response) {
+    var msg = '※伝言板を表示します';
+    var cookie_data = getCookie('msg', request);
     var content = ejs.render(index_page, {
         title: "Index",
         content: msg,
-        data:data,
+        data: data,
+        cookie_data:cookie_data,
     });
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(content);
     response.end();
 }
 
+//クッキーの値を設定
+function setCookie(key, value, response) {
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+//クッキーの値を取得
+function getCookie(key, request) {
+    var rhCookie = request.headers.cookie;
+    var cookie_data = 
+    rhCookie != undefined?　rhCookie : ``; //クッキーがundefindeなら空のテキストを取出す
+    var data = cookie_data.split(';');//配列にする
+    for(var i in data){
+        if (data[i].trim().startsWith(key + '=')) {//前後の空白消すkey=で始まるかチェック
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);//クッキーの普通のテキストに戻す
+        }
+    }
+    return '';
+}
 //Otherのアクセス処理
+var data2 = {
+    'Taro': ['taro@yamada.com', '09-0999-333', 'Tokyo'],
+    'Hanako': ['Hanako@miyashita.com', '09-09990-453', 'Sapporo'],
+    'Maruto': ['Maruto@sawada.com', '09-099-8888', 'NewYork'],
+    'Katuro': ['Katuro@isono.com', '09-0923-555', 'Hukushima']
+};
+
 function response_other(request, response) {
     var msg = 'これはOtherページです。';
 
@@ -77,7 +124,7 @@ function response_other(request, response) {
 
         //データ受信のイベント処理
         request.on('data', (data) => { //引数には受けとった(入力された)データが入ってる
-            body += data; 
+            body += data;
         });
 
         //データ受信終了のイベント処理
@@ -87,6 +134,8 @@ function response_other(request, response) {
             var content = ejs.render(other_page, {
                 title: 'Other',
                 content: msg,
+                data: data2,
+                filename: 'data_item'
             });
             response.writeHead(200, { 'Content-Type': 'text/html' });
             response.write(content);
