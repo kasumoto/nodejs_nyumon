5d1378961f356cb5ee04c3f5dda37a290f182615
kasumoto <hasmoto.0112@gmail.com>
2020-09-26 15:15:13 +0900

    Finish Chapter05

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//sessionの設定
+var session =require('express-session');
+var session_opt = {
+  secret: 'keyboard vat',//秘密キー
+  resave: false,//セッションストアに値を強制保存
+  saveUnittialized: false,
+  cookie: { maxAge: 60 * 60 * 1000} //セッションの保持時間
+};
+//var jquery = require('express-jquery');
+var ajax = require('./routes/ajax');
+ 
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,11 +26,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//app.useで利用する文は、ルーティングのためのapp.useよりも前に書く
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(session_opt));
+//app.use(jquery('/jquery'));
+app.use('/ajax', ajax);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,5 +56,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,12 +1,114 @@
+const { request } = require('express');
 var express = require('express');
 var router = express.Router();
+var http = require('https');
+var parseString = require('xml2js').parseString;
+var sqlite3 = require('sqlite3');
 
-router.get('/',(req, res, next) => {
-    var data = {
-        title: 'Hello! Page',
-        content: 'Hello,Express',        
-    };
-    res.render('hello',data);
+//データベースオブジェクトの取得
+var db = new sqlite3.Database('mydb.sqlite3');
+
+router.get('/',(req, res, next) => { //初期アクセス時
+    //データベースのシリアライズ
+    db.serialize(() => {
+        //レコードを全て取り出す
+        db.all("select * from mydata",(err, rows) => {
+            //データベースアクセス完了時の処理
+            if(!err) {
+                var data = {
+                    title: 'Hello!',
+                    content: rows //取得したレコードデータ
+                };
+                res.render('hello/index',data);
+            }
+        });
+    });
 });
+
+router.get('/add', (req, res, next) => {
+    var data = {
+        title: 'Hello/Add' ,
+        content: '新しいコードを入力:'
+    }
+    res.render('hello/add', data);
+});
+
+router.post('/add', (req, res, next)  => {
+    var nm = req.body.name;
+    var ml = req.body.mail;
+    var ag = req.body.age;
+    db.run('insert into mydata (name, mail, age) values (?, ?, ?)',
+    nm,ml,ag);
+    res.redirect('/hello');
+});
+
+router.get('/show', (req, res, next) => {
+    var id =req.query.id;
+    db.serialize(() => {
+        var q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err,row) => {
+            if(!err) {
+                var data = {
+                    title: 'Hello/show',
+                    content: 'id = ' + id + 'のレコード',
+                    mydata: row
+                }
+                res.render('hello/show', data);
+            }
+        });
+    });
+});
+
+router.get('/edit', (req,res, next) => {
+    var id = req.query.id;
+    db.serialize( () => {
+        var q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err,row) => {
+            if (!err) {
+                var data = {
+                    title: 'hello/edit',
+                    content: 'id = ' + id + 'レコードの編集:',
+                    mydata: row
+                }
+                res.render('hello/edit', data);
+            }
+        });
+    });
+});
+
+router.post('/edit', (req,res, next) => {
+    var id = req.body.id;
+    var nm = req.body.name;
+    var ml = req.body.mail;
+    var ag = req.body.age;
+    var q = 'update mydata set name = ?, mail = ?, age = ? where id = ?';
+    db.run(q,nm,ml,ag,id);
+    res.redirect('/hello');
+});
+
+router.get('/delete', (req,res, next) => {
+    var id = req.query.id;
+    db.serialize( () => {
+        var q = 'select * from mydata where id = ?';
+        db.get(q, [id], (err,row) => {
+            if (!err) {
+                var data = {
+                    title: 'hello/delete',
+                    content: 'id = ' + id + 'レコードの削除:',
+                    mydata: row
+                }
+                res.render('hello/delete', data);
+            }
+        });
+    });
+});
+
+router.post('/delete', (req,res, next) => {
+    var id = req.body.id;
+    var q = 'delete from mydata where id = ?';
+    db.run(q, id);
+    res.redirect('/hello');
+});
+
 
 module.exports = router;
